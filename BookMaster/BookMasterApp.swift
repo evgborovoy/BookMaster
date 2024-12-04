f3c75e6805f607b5a70a0337b63e3cc623f323cd
Evgeniy <borovoyevgen@gmail.com>
2024-12-04 11:25:14 +0400

    Initial Commit

---
+++ BookMaster/BookMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookMasterApp.swift
+//  BookMaster
+//
+//  Created by Evgeniy Borovoy on 12/4/24.
+//
+
+import SwiftUI
+
+@main
+struct BookMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookMaster/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BookMaster
+//
+//  Created by Evgeniy Borovoy on 12/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
